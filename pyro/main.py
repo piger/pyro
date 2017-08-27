e62be423c67a60ffeee2418b51636af5251217ec
Daniel Kertesz <dkertesz@zendesk.com>
2017-08-28 00:34:44 +0100

    options

--- pyro/main.py
+++ pyro/main.py
@@ -6,13 +6,15 @@ from pyro.game import Game
 def main():
     parser = OptionParser()
     parser.add_option('--seed', '-s')
-    opts, args = parser.parse_args()
+    parser.add_option('--width', '-W', type=int, default=80)
+    parser.add_option('--height', '-H', type=int, default=60)
+    (opts, _) = parser.parse_args()
     if opts.seed is None:
         seed = int(time.time())
     else:
         seed = opts.seed
 
-    game = Game(seed)
+    game = Game(seed, opts.width, opts.height)
     game.init_game()
     game.game_loop()
 
