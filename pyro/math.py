4fa82a2f30a0c3c71fa997ba195940a4f06386aa
Daniel Kertesz <dkertesz@zendesk.com>
2017-08-28 11:20:09 +0100

    new rect class

--- pyro/math.py
+++ pyro/math.py
@@ -19,3 +19,27 @@ NORTH = Vector2(0, -1)
 SOUTH = Vector2(0, 1)
 EAST = Vector2(1, 0)
 WEST = Vector2(-1, 0)
+
+
+
+class Rect(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.endX = self.x + self.width
+        self.endY = self.y + self.height
+
+    @property
+    def center(self):
+        center_x = int((self.x + self.endX) / 2)
+        center_y = int((self.y + self.endY) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        return (self.x <= other.endX and self.endX >= other.x and
+                self.y <= other.endY and self.endY >= other.y)
+
+    def __repr__(self):
+        return 'Rect(x=%d, y=%d, width=%d, height=%d)' % (self.x, self.y, self.width, self.height)
